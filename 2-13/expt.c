db8d42f8b1adfc21875a720374facfc055a384e4
Houfu Yan <yanhoufu@cs.grinnell.edu>
2017-02-13 12:59:54 -0600

    reading 2-13

---
+++ 2-13/expt.c
@@ -0,0 +1,42 @@
+double expt( double base, int power)
+{
+
+  double answer = base;
+
+  if(power==0)
+    {
+      return 1;
+    }
+  else if(power <0)
+    {
+      power = -power;
+        for(int x = 0; x<power; x++)
+          {
+            answer = answer*base;
+          }
+      return 1/answer;
+    }
+  else
+    {
+
+      for(int x = 0; x<power; x++)
+        {
+          answer = answer*base;
+        }
+      return answer;
+    }
+
+
+}
+
+
+
+int main(void)
+{
+  double a = expt(2, 3);
+  double b = expt(-3, 3);
+  double c = expt(3, -4);
+  double d = expt(0, 5);
+  double e = expt(.23, 4);
+  return 0;
+}
